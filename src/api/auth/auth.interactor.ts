9cd3637be52919007241fefdfc35326235ec7f36
nazarzastavnyi <nazar.zastavniy@dashdevs.com>
2024-03-26 12:17:50 +0100

    Fix nest dependencies

--- src/api/auth/auth.interactor.ts
+++ src/api/auth/auth.interactor.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { IUserRegistrationGateway } from '@common/gateways/interfaces/i-user-registration.gateway';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  IUserRegistrationGateway,
+  UserRegistrationGatewayType,
+} from '@common/gateways/interfaces/i-user-registration.gateway';
 import { IRegisterCommand } from '@api/auth/interfaces/i-register.command';
 
 @Injectable()
 export class AuthInteractor {
   constructor(
+    @Inject(UserRegistrationGatewayType)
     private readonly userRegistrationGateway: IUserRegistrationGateway,
   ) {}
 
